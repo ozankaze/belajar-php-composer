03a5f255c940f08518ed86f8adfa20d8e07ed145
ozankaze <ozankaze101@gmail.com>
2023-06-08 22:08:26 +0700

    Menambah Dependency

---
+++ HelloTimer.php
@@ -0,0 +1,24 @@
+<?php
+
+// 32
+
+require __DIR__ . '/vendor/autoload.php';
+
+use SebastianBergmann\Timer\Timer;
+
+$timer = new Timer;
+
+$timer->start();
+
+foreach (\range(0, 100000) as $i) {
+    // ...
+}
+
+$duration = $timer->stop();
+
+var_dump(get_class($duration));
+var_dump($duration->asString());
+var_dump($duration->asSeconds());
+var_dump($duration->asMilliseconds());
+var_dump($duration->asMicroseconds());
+var_dump($duration->asNanoseconds());
